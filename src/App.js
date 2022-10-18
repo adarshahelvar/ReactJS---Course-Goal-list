6e1c7f8b3f70637d19453ae6452eaf866cc5d37b
Adarsha A <adarshaadi1997@gmail.com>
2022-10-18 10:37:27 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ const App = () => {
   const addGoalHandler = enteredText => {
     setCourseGoals(prevGoals => {
       const updatedGoals = [...prevGoals];
-      updatedGoals.unshift({ text: enteredText, id: 'goal1' });
+      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
       return updatedGoals;
     });
   };
 
-  const deleteItemHandler = goalId => {
+  const deleteItemHandler = (goalId) => {
     setCourseGoals(prevGoals => {
       const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
       return updatedGoals;
